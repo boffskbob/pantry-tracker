a3d1a7275f0ff84502b9f77eb97d80874c4f8ce2
boffskbob <wab5rt@virginia.edu>
2024-08-04 21:13:29 -0400

    add searchbar functionality

--- app/page.js
+++ app/page.js
@@ -2,13 +2,15 @@
 import Image from "next/image";
 import { useState, useEffect, use } from 'react'
 import { firestore } from "@/firebase"
-import { Box, Typography, Modal, Stack, TextField, Button } from '@mui/material'
+import { Box, Typography, Modal, Stack, TextField, Button, Grid } from '@mui/material'
 import { collection, doc, getDocs, query, deleteDoc, getDoc, setDoc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredItems, setFilteredItems] = useState();
 
   const updateInventory = async () => {
     // creates a query object
@@ -28,7 +30,7 @@ export default function Home() {
 
   const addItem = async (item) => {
     // get direct item reference
-    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docRef = doc(collection(firestore, 'inventory'), item.toLowerCase())
     const docSnap = await getDoc(docRef)
 
     if (docSnap.exists()) {
@@ -60,13 +62,28 @@ export default function Home() {
     await updateInventory()
   }
 
-  const handleOpen = () => { setOpen(true) }
-  const handleClose = () => { setOpen(false) }
+  const updateSearch = async () => {
+    return (inventory.filter((obj) => {
+      return obj.name.toLowerCase() === searchInput.toLowerCase()
+    }))
+  }
+
+  // add image
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
 
   useEffect(() => {
-    updateInventory()
-  }, [])
+    const filterItems = async () => {
+      await updateInventory()
+      const filtered = inventory.filter(item =>
+        item.name.toLowerCase().includes(searchInput.toLowerCase())
+      );
+      await setFilteredItems(filtered);
+    }
+    filterItems()
+  }, [searchInput])
   return (
     <Box
       width="100vw"
@@ -78,7 +95,7 @@ export default function Home() {
       alignItems="center"
       gap={2}
     >
-      <Modal open={open} onClose={() => handleClose()}>
+      <Modal open={open} onClose={() => { handleClose() }}>
         <Box
           position="absolute"
           top="50%"
@@ -114,33 +131,47 @@ export default function Home() {
 
         </Box>
       </Modal>
-      <Box border="1px solid #333">
-        <Box width="800px" height="100px" bgcolor="#ADD8E6" alignItems="center" justifyContent="center" display="flex">
-          <Typography variant="h2" color="#333"> Inventory Items </Typography>
-        </Box>
-        <Stack width="800px" height="300px" spacing={2} overflow="auto">
-          <Box key="header" width="100%" minHeight="150px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" padding={5}>
+      <Grid item xs={3} display="flex" alignItems="center" justifyContent="center" flexDirection="column" gap={2}>
+        <Box border="1px solid #333">
+          <Box width="800px" height="100px" bgcolor="#ADD8E6" alignItems="center" justifyContent="center" display="flex">
+            <Typography variant="h2" color="#333"> Inventory Items </Typography>
+          </Box>
+          <Box key="header" width="100%" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#ADD8E6" padding={5} paddingY={2}>
             <Typography variant='h4' color="#333" textAlign="center" >Item</Typography>
             <Typography variant='h4' color="#333" textAlign="center" >Count</Typography>
             <Typography variant='h4' color="#333" textAlign="center" >Edit</Typography>
           </Box>
-          {inventory.map(({ name, quantity }) => (
-            <Box key={name} width="100%" minHeight="150px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" padding={5}>
-              <Typography variant='h4' color="#333" textAlign="center" >{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
-              <Typography variant='h4' color="#333" textAlign="center" >{quantity}</Typography>
-              <Stack direction="column" spacing={1}>
-                <Button variant="contained" onClick={() => { addItem(name) }} size="small">Add</Button>
-                <Button variant="contained" onClick={() => { removeItem(name) }} size="small">Remove</Button>
-              </Stack>
-            </Box>
-          ))}
-        </Stack>
-      </Box>
-      <Button variant="contained" onClick={() => {
-        handleOpen()
-      }}>
-        Add new Item
-      </Button>
-    </Box>
+          {/* Search */}
+          <Box >
+            <TextField
+              variant="outlined"
+              fullWidth
+              placeholder="Search items..."
+              value={searchInput}
+              onChange={(e) => {
+                setSearchInput(e.target.value)
+              }} />
+          </Box>
+          {/* Table */}
+          <Stack width="800px" height="300px" spacing={2} overflow="auto">
+            {(!Array.isArray(filteredItems) || !filteredItems.length ? inventory : filteredItems).map(({ name, quantity }) => (
+              <Box key={name} width="100%" minHeight="100px" display="flex" alignItems="center" justifyContent="space-between" bgcolor="#f0f0f0" padding={5} paddingY={2}>
+                <Typography variant='h4' color="#333" textAlign="center" >{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
+                <Typography variant='h4' color="#333" textAlign="center" >{quantity}</Typography>
+                <Stack direction="column" spacing={1}>
+                  <Button variant="contained" onClick={() => { addItem(name) }} size="small">Add</Button>
+                  <Button variant="contained" onClick={() => { removeItem(name) }} size="small">Remove</Button>
+                </Stack>
+              </Box>
+            ))}
+          </Stack>
+        </Box>
+        <Button variant="contained" onClick={() => {
+          handleOpen()
+        }}>
+          Add new Item
+        </Button>
+      </Grid>
+    </Box >
   );
 }
